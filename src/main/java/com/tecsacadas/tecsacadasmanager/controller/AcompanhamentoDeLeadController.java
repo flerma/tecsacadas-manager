9bd854ee25c5c3d669cb7fd36988ca89041055be
Fernando Marchesano Lerma <fernandolerma@macro.com.ar>
2024-01-02 10:53:43 -0300

    Swagger

--- src/main/java/com/tecsacadas/tecsacadasmanager/controller/AcompanhamentoDeLeadController.java
+++ src/main/java/com/tecsacadas/tecsacadasmanager/controller/AcompanhamentoDeLeadController.java
@@ -3,6 +3,13 @@ package com.tecsacadas.tecsacadasmanager.controller;
 import com.tecsacadas.tecsacadasmanager.exception.FileNotSupportedException;
 import com.tecsacadas.tecsacadasmanager.service.AcompanhamentoDeLeadService;
 import com.tecsacadas.tecsacadasmanager.validator.UploadValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,15 +30,36 @@ public class AcompanhamentoDeLeadController {
     private final AcompanhamentoDeLeadService acompanhamentoLeadsService;
     private final UploadValidator uploadValidator;
 
+    @Operation(summary = "Importar planilha de leads",
+            description = "Permite importar a planilha de leads para a base de dados permitindo a geração de reletórios relacionados.")
+    @ApiResponse(responseCode = "200",
+            description = "Sucesso")
+    @ApiResponse(responseCode = "404",
+            description = "Solicitud no encontrada")
     @GetMapping("/importar")
     public ResponseEntity<Void> importar() {
         acompanhamentoLeadsService.importar();
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Importar planilha de leads",
+            description = "Permite importar a planilha de leads para a base de dados permitindo a geração de reletórios relacionados.")
+    @Parameter(name = "file",
+               description = "Arquivo excel de leads a ser importado.",
+               required = true,
+               extensions = @Extension(name = "allowedTypes",
+                                       properties = @ExtensionProperty(name = "xlsx",
+                                                                       value = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")),
+               content = @Content(mediaType = "multipart/form-data",
+                                  schema = @Schema(type = "multipart/form-data")),
+               example = "leads.xlsx")
+    @ApiResponse(responseCode = "200",
+            description = "Sucesso")
+    @ApiResponse(responseCode = "400",
+            description = "Arquivo ou formato errado")
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ResponseEntity<Void> upload(@RequestPart("file") MultipartFile file) {
-        uploadValidator.validate(file);
+        UploadValidator.validate(file);
         acompanhamentoLeadsService.upload(file);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/tecsacadas/tecsacadasmanager/validator/UploadValidator.java
+++ src/main/java/com/tecsacadas/tecsacadasmanager/validator/UploadValidator.java
@@ -1,14 +1,20 @@
 package com.tecsacadas.tecsacadasmanager.validator;
 
 import com.tecsacadas.tecsacadasmanager.exception.FileNotSupportedException;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
+@NoArgsConstructor
 public class UploadValidator {
 
     public static void validate(MultipartFile file) {
         String contentType = file.getContentType();
+
+        if (contentType == null)
+            throw new FileNotSupportedException("Tipo de arquivo não suportado. Por favor, envie um arquivo Excel.");
+
         if (!contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
             throw new FileNotSupportedException("Tipo de arquivo não suportado. Por favor, envie um arquivo Excel.");
         }
