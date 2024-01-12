0c6b5e221d2e1453eee6dfe5055794c465679c0c
Fernando Marchesano Lerma <fernandolerma@macro.com.ar>
2024-01-12 00:44:37 -0300

    Funcionalidades de relatorios

--- src/main/resources/db/migration/V5__create_table_relatorios.sql
+++ src/main/resources/db/migration/V5__create_table_relatorios.sql
@@ -3,5 +3,5 @@ CREATE TABLE report (
     source        			 VARCHAR(255) NOT NULL,
     name         			 VARCHAR(255) NOT NULL,
     identifier   		     VARCHAR(255) NOT NULL,
-    CONSTRAINT relatorio_pkey PRIMARY KEY (id)
-);
+    CONSTRAINT report_pkey PRIMARY KEY (id)
+) CHARACTER SET utf8mb4,  COLLATE utf8mb4_unicode_ci;
